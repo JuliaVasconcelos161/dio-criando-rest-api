7bbceca9f20cdbc6c285f1e1a37fd16c6bc3aa5e
Julia <juliavasconceloscr@gmail.com>
2023-02-02 08:55:52 -0300

    Primeiro Commit Projeto Criado

---
+++ src/main/java/com/github/juliavasconceloscr161/diocriandorestapi/DioCriandoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.juliavasconceloscr161.diocriandorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioCriandoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioCriandoRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/juliavasconceloscr161/diocriandorestapi/DioCriandoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juliavasconceloscr161.diocriandorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioCriandoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
